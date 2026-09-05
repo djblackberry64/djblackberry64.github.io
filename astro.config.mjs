4d3c1410d02a8ad96a22a599246825fe71e0eef7
djblackberry64 <220242469+djblackberry64@users.noreply.github.com>
2026-09-05 23:21:31 +0200

    Change site attribute in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://djblackberry64.github.io/",
+  site: "https://djblackberry64.github.io",
   integrations: [
     mdx(),
     sitemap(),
