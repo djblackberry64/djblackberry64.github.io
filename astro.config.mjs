7c15df2d50521c6e03f03ba778babdffc5da25f2
djblackberry64 <220242469+djblackberry64@users.noreply.github.com>
2026-09-06 12:38:12 +0200

    Remove sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://djblackberry64.github.io",
-  integrations: [
-    mdx(),
-    // sitemap(),
-    solidJs(),
-    tailwind({ applyBaseStyles: false }),
-  ],
+  integrations: [mdx(), solidJs(), tailwind({ applyBaseStyles: false })],
 });
